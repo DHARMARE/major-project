be777c48675d0f79b513d3d4b20b15d85ebe836e
DHARMARE <104313012+DHARMARE@users.noreply.github.com>
2024-01-25 07:18:01 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,43 @@
+function loadinganimation(){
+    gsap.from("#main #hero h1",{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    // stagger:0.3
+})
+}
+loadinganimation()
+
+function loadingimg1(){
+    gsap.from("#course1 img",{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    // stagger:0.3
+})
+}
+loadingimg1()
+
+function loadingimg2(){
+    gsap.from("#course2 img",{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    // stagger:0.3
+})
+}
+loadingimg2()
+
+function loadingimg3(){
+    gsap.from("#course3 img",{
+    y:100,
+    opacity:0,
+    delay:0.5,
+    duration:0.9,
+    // stagger:0.3
+})
+}
+loadingimg3()
