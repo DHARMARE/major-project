b0bf62a06abd65d2c92ed4878d6f7d84dec066e9
DHARMARE <104313012+DHARMARE@users.noreply.github.com>
2024-04-05 12:40:58 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,79 @@
+function loadinganimation() {
+    gsap.from("#main #hero h1", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadinganimation()
+
+function loadingimg1() {
+    gsap.from("#course1 img", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingimg1()
+
+function loadingimg2() {
+    gsap.from("#course2 img", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingimg2()
+
+function loadingimg3() {
+    gsap.from("#course3 img", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingimg3()
+
+function navigateToAnotherPage() {
+    // Replace 'targetPage.html' with the actual name of the page you want to navigate to
+    window.location.href = "index3.html";
+}
+
+function loadingtxt1() {
+    gsap.from("#course1 h5", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingtxt1()
+function loadingtxt2() {
+    gsap.from("#course2 h5", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingtxt2()
+function loadingtxt3() {
+    gsap.from("#course3 h5", {
+        y: 100,
+        opacity: 0,
+        delay: 0.5,
+        duration: 0.9,
+        // stagger:0.3
+    })
+}
+loadingtxt3()
---
+++ script3.js
@@ -0,0 +1,4 @@
+function navigateToAnotherPage() {
+    // Replace 'targetPage.html' with the actual name of the page you want to navigate to
+    window.location.href = "index4.html";
+}
